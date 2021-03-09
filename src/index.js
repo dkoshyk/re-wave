5cae9a6e3364c92d1d6fa0a992c3e4467a4bd787
DmytroStrembetskyi <dmytrostrembetskyi@gmail.com>
2021-03-09 11:46:22 +0200

    refactor redux store

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import configureStore from './redux/store';
 import { Provider } from 'react-redux';
+import { configureStore } from './redux/store';
 
 const store = configureStore();
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState);
+export function configureStore() {
+    return createStore(rootReducer);
 }
